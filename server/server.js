52e44a6ac695dfd7ea5d9c78719f5bbf9a77dd68
John Dave Villasan <dave.uc.main.it@gmail.com>
2026-03-18 10:08:45 +0800

    moved packaged and server js to folder

---
+++ server/server.js
@@ -0,0 +1,118 @@
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const cors = require("cors");
+
+const app = express();
+const PORT = 3000;
+const SECRET_KEY = "your-very-secure-secret";
+
+app.use(
+  cors({
+    origin: ["http://127.0.0.1:5500", "http://localhost:5500"],
+  }),
+);
+
+app.use(express.json());
+
+let users = [
+  {
+    id: 1,
+    username: "admin",
+    password: "$2a$10$...",
+    role: "admin",
+  },
+
+  {
+    id: 2,
+    username: "alice",
+    password: "$2a$10$...",
+    role: "user",
+  },
+];
+
+if (!users[0].password.includes("$2a$")) {
+  users[0].password = bcrypt.hashSync("admin123", 10);
+  users[1].passwprd = bcrypt.hashSync("user123", 10);
+}
+
+app.post("/api/register", async (req, res) => {
+  const { username, password, role = "user" } = req.body;
+
+  if (!username || !password) {
+    return req
+      .statusCode(400)
+      .json({ error: "Username and password required" });
+  }
+
+  const existing = users.find((u) => u.username === username);
+  if (existing) {
+    return res.status(409).json({ error: "User already exists" });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const newUser = {
+    id: users.length + 1,
+    username,
+    password: hashedPassword,
+    role,
+  };
+
+  users.push(newUser);
+  res.status(201).json({ message: "User registered", username, role });
+});
+
+app.post("/api/login", async (req, res) => {
+  const user = users.find((u) => u.username === username);
+  if (!user || !(await bcrypt.compare(password, user.password))) {
+    return res.status(401).json({ error: "Invalid credentials" });
+  }
+
+  const token = jwt.sign(
+    {
+      id: user.id,
+      username: user.username,
+      role: user.role,
+    },
+    SECRET_KEY,
+    { expiresIn: "1h" },
+  );
+  res.json({ token, user: { username: user.username, role: user.role } });
+});
+
+app.get("/api/profile", authenticateToken, (req, res) => {
+  res.json({ user: req.user });
+});
+
+app.get(
+  "/api/admin/dashboard",
+  authenticateToken,
+  authorizeRole("admin"),
+  (req, res) => {
+    res.json({
+      message: "Welcome to admin dashboard!",
+      data: "Secret admin info",
+    });
+  },
+);
+
+app.get("/api/content/guest", (req, res) => {
+  res.json({ message: "Public content for all visitors " });
+});
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ error: "Access token required" });
+  }
+
+  jwt.verify(token, SECRET_KEY, (err, user) => {
+    if (err) return res.status(403).json({ error: "Invalid or expired token" });
+    req.user = user;
+    next();
+  });
+}
